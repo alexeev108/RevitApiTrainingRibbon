d743ac38c52b33bdd6c990cfeeac630519d2992a
alex206 <alexeev108@yandex.ru>
2025-10-15 22:07:10 +0300

    feat: сделана Лаб.5.3

--- RevitApiTrainingRibbon/Main.cs
+++ RevitApiTrainingRibbon/Main.cs
@@ -23,19 +23,25 @@ namespace RevitApiTrainingRibbon
         {
             string tabName = "RevitApiTraining";
             application.CreateRibbonTab(tabName);
-            string utilsFolderPath = @"C:\Users\alex206\Desktop\RevitApiTrainingUI";
+            string utilsFolderPath_1 = @"C:\Users\alex206\Desktop\RevitApiTrainingUIElem";
+            string utilsFolderPath_2 = @"C:\Users\alex206\Desktop\RevitApiTrainingUIWall";
 
-            var panel = application.CreateRibbonPanel(tabName, "Трубы");
+            var panel = application.CreateRibbonPanel(tabName, "Стены");
 
-            var button = new PushButtonData("Система", "Смена системы труб",
-                Path.Combine(utilsFolderPath, "RevitApiTrainingUI.dll"),
+            var button_1 = new PushButtonData("Информация об элементах", "Информация об элементах",
+                Path.Combine(utilsFolderPath_1, "RevitApiTrainingUI.dll"),
+                "RevitApiTrainingUI.Main");
+
+            var button_2 = new PushButtonData("Смена типа стены", "Смена типа стены",
+                Path.Combine(utilsFolderPath_2, "RevitApiTrainingUI.dll"),
                 "RevitApiTrainingUI.Main");
 
             Uri uriImage = new Uri(@"C:\Users\alex206\Desktop\Курсы\ИТМО. Автоматизация информационного поделирования\Модуль 5\RevitApiTrainingRibbon\RevitApiTrainingRibbon\Images\RevitAPITrainingUI_32.png", UriKind.Absolute);
             BitmapImage largeImage = new BitmapImage(uriImage);
-            button.LargeImage = largeImage;
+            button_2.LargeImage = largeImage;
 
-            panel.AddItem(button);
+            panel.AddItem(button_1);
+            panel.AddItem(button_2);
 
             return Result.Succeeded;
         }
